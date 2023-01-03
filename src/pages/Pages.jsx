acec5e6782dbc870024411f5a14bee64bfd364b0
sestradarya <nguendarina1@gmail.com>
2023-01-03 20:13:02 +0300

    recipe-react-app

---
+++ src/pages/Pages.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Cuisine } from "./Cuisine";
+import Home from "./Home";
+
+function Pages() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cuisine" element={<Cuisine />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Pages;
